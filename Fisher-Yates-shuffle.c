4f335e905a1872be033972d5da8d3b7a48a96508
zzzxxx00019 <zzzxxx00019@gmail.com>
2020-09-17 09:29:16 +0800

    add shuffle.c

---
+++ Fisher-Yates-shuffle.c
@@ -0,0 +1,87 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct __node {
+    int value;
+    struct __node *next;
+} node_t;
+
+void add_entry(node_t **head, int new_value)
+{
+    node_t **indirect = head;
+
+    node_t *new_node = malloc(sizeof(node_t));
+    new_node->value = new_value;
+    new_node->next = NULL;
+
+    assert(new_node);
+    while (*indirect)
+        indirect = &(*indirect)->next;
+    *indirect = new_node;
+}
+
+void shuffle(node_t **head) {
+  if (!*head)
+    return;
+
+  int length = 0;
+  node_t **tmp_head = head;
+
+  for (; *tmp_head ; tmp_head = &(*tmp_head)->next, length += 1);
+
+  srand(time(NULL));
+  tmp_head = head ;
+
+  while (length > 1) {
+    int Roll = rand() % length + 1;
+    node_t **indirect = tmp_head;
+
+    for(; Roll!=1 ; Roll--, indirect=&(*indirect)->next) ;
+/*
+    while (Roll != 1) {
+      indirect = &(*indirect)->next;
+      Roll--;
+    }
+*/
+    int tmp_value = (*tmp_head)->value;
+    (*tmp_head)->value = (*indirect)->value;
+    (*indirect)->value = tmp_value;
+
+    tmp_head = &(*tmp_head)->next;
+
+    length -= 1;
+  }
+}
+
+void print_list(node_t *head)
+{
+    while(head)
+    {
+        printf("%d ",head->value) ;
+        head = head->next ;
+    }
+    printf("\n") ;
+}
+
+int main()
+{
+    node_t *head = NULL;
+
+    print_list(head);
+
+    add_entry(&head, 72);
+    add_entry(&head, 101);
+    add_entry(&head, 108);
+    add_entry(&head, 109);
+    add_entry(&head, 110);
+    add_entry(&head, 111);
+
+    print_list(head);
+    shuffle(&head);
+    print_list(head);
+
+    return 0;
+}
+
